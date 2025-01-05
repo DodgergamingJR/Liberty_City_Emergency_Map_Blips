43074851003959692fff47093ab0e5f621d728df
charlieflockhart <DodgergamingJR@yahoo.co.uk>
2025-01-05 00:58:56 +0000

    V1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 title "Liberty_City_Emergency_Map_Blips"
 author "DodgergamingJR" -- Discord
 description "A script Displaying Blips On Liberty City"
-version "1.0.0"
+version "1.0.1"
 
 server_scripts {
     'server/*.lua'
